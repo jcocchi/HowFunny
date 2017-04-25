cadd04accee6ff7fad8691ec07c2c5be31499346
Justine Cocchi <jucocchi@microsoft.com>
2017-04-25 17:10:10 -0500

    created azure function to call youtube

---
+++ YoutubeDataExtraction/index.js
@@ -0,0 +1,26 @@
+var YouTube = require('youtube-node')
+
+module.exports = function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.')
+  var category = 'comedy' // this is our default
+
+  // Search YouTube for the category we're looking for
+  if (req.query.name || (req.body && req.body.name)) {
+    category = (req.query.name || req.body.name)
+  }
+
+  // Setup YouTube connection
+  var youtube = new YouTube()
+  youtube.setKey('AIzaSyBO16-NgI5Lx8gPytGvsa2Hqb7rNftArCA')
+
+  // Get video data from API
+  youtube.search(category, 50, function (error, result) {
+    if (error) {
+      context.log(error)
+      context.done()
+    } else {
+      context.log(JSON.stringify(result, null, 2))
+      context.done()
+    }
+  })
+}
