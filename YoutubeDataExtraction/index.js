3f3070f875fc7969f2653c56492cff9dac23ab16
Justine Cocchi <jucocchi@microsoft.com>
2017-04-26 10:47:02 -0500

    added youtube data json file

--- YoutubeDataExtraction/index.js
+++ YoutubeDataExtraction/index.js
@@ -1,4 +1,28 @@
-var YouTube = require('youtube-node')
+// var YouTube = require('youtube-node')
+var request = require('request')
+var queryString = require('querystring')
+var url = 'https://www.googleapis.com/youtube/v3/search?'
+
+var makeRequest = function (url, callback) {
+  request(url, function (error, response, body) {
+    if (error) {
+      callback(error)
+    } else {
+      var data = {}
+      try {
+        data = JSON.parse(body)
+      } catch (e) {
+        data = {}
+      }
+
+      if (response.statusCode == 200) {
+        callback(null, data)
+      } else {
+        callback(data.error)
+      }
+    }
+  })
+}
 
 module.exports = function (context, req) {
   context.log('JavaScript HTTP trigger function processed a request.')
@@ -9,12 +33,16 @@ module.exports = function (context, req) {
     category = (req.query.name || req.body.name)
   }
 
-  // Setup YouTube connection
-  var youtube = new YouTube()
-  youtube.setKey('AIzaSyBO16-NgI5Lx8gPytGvsa2Hqb7rNftArCA')
+  // Set up query parameters
+  var params = {
+    key: 'AIzaSyBO16-NgI5Lx8gPytGvsa2Hqb7rNftArCA',
+    path: 'snippet,statistics,contentDetails',
+    q: category,
+    maxResults: 20
+  }
+  var reqUrl = url + queryString.stringify(params)
 
-  // Get video data from API
-  youtube.search(category, 50, function (error, result) {
+  makeRequest(reqUrl, function (error, result) {
     if (error) {
       context.log(error)
       context.done()
@@ -23,4 +51,19 @@ module.exports = function (context, req) {
       context.done()
     }
   })
+
+  // // Setup YouTube connection
+  // var youtube = new YouTube()
+  // youtube.setKey('AIzaSyBO16-NgI5Lx8gPytGvsa2Hqb7rNftArCA')
+
+  // // Get video data from API
+  // youtube.search(category, 50, function (error, result) {
+  //   if (error) {
+  //     context.log(error)
+  //     context.done()
+  //   } else {
+  //     context.log(JSON.stringify(result, null, 2))
+  //     context.done()
+  //   }
+  // })
 }
